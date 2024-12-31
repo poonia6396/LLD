3b5423e6e8f71565c0e46f4900748f008a3c963e
Nishant Poonia <nishant.poonia6396@gmail.com>
2024-12-31 11:12:39 +0530

    Update TicTacToe.py

--- Tic-Tac-Toe/TicTacToe.py
+++ Tic-Tac-Toe/TicTacToe.py
@@ -5,17 +5,6 @@ class TicTacToe:
         self.players = players
         self.winner_calculator = winner_calculator
     
-    def is_valid_move_cordinates(self, move_cordinates):
-        if len(move_cordinates) != 2:
-            return False
-            
-        if not(move_cordinates[0].isdigit() and int(move_cordinates[0]) < self.board.length):
-            return False
-        
-        if not(move_cordinates[1].isdigit() and int(move_cordinates[0]) < self.board.breadth):
-            return False
-
-        return True
 
     def is_there_a_winner(self, move_cordinates):
         return self.winner_calculator.is_there_a_winner(move_cordinates, self.board)
@@ -35,7 +24,7 @@ class TicTacToe:
             player_input = input("\n\n"+current_player.name+" please enter your move cordinates: (e.g row,column)\n")
             move_cordinates = player_input.split(",")
 
-            if not self.is_valid_move_cordinates(move_cordinates) or not self.board.register_move(move_cordinates, current_player.player_piece):
+            if not self.board.register_move(move_cordinates, current_player.player_piece):
                 print("Not a valid move. Please retry")
                 self.players.insert(0, current_player)
                 continue
